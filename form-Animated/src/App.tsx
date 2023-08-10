40eff1081bf831166f7ab8513a5a3f16ebbcb4ba
jonas mendes <jonas.jsm2903@gmail.com>
2023-08-10 11:50:03 -0300

    instalando projeto e criando o header sem estilização

---
+++ form-Animated/src/App.tsx
@@ -0,0 +1,20 @@
+import './App.scss'
+
+function App() {
+
+  return (
+  <main>
+    <header className='header-container'> 
+      <img src="./logo-img.svg" alt=""/>
+      <nav className='menu-options'> 
+        <a href="">Oque nos fazemos</a>
+        <a href="">Como funciona</a>
+        <a href="">Portifolio</a>
+      </nav>
+      <button>Nos contact</button>
+    </header>
+  </main>
+  )
+}
+
+export default App
