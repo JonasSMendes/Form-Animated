b92df55fd1550e71b091372b1e863a7796a05f83
jonas mendes <jonas.jsm2903@gmail.com>
2023-08-11 11:48:55 -0300

    tela principal com menu e animação

--- form-Animated/src/App.tsx
+++ form-Animated/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.scss'
+import { Arrowicon } from './components/arrow-icon'
 
 function App() {
 
@@ -11,8 +12,35 @@ function App() {
         <a href="">Como funciona</a>
         <a href="">Portifolio</a>
       </nav>
-      <button>Nos contact</button>
+      <button>
+        <div className='btn-face-white'>
+          Nos contact
+          <Arrowicon/>
+        </div>
+        <div className='btn-face-purple'>
+          Nos contacte
+          <Arrowicon/>
+        </div>
+      </button>
     </header>
+    <div className='content-container'>
+      <div className='heading'>
+        <h2>Vamos crescer sua</h2>
+        <h2> presença social </h2>
+      </div>
+      <div className='buble target'>
+        🎯
+      </div>
+      <div className='buble shopp'>
+        🛍️
+      </div>
+      <div className='buble rocket'>
+        🚀
+      </div>
+      <div className='buble fire'>
+        🔥
+      </div>
+    </div>
   </main>
   )
 }
